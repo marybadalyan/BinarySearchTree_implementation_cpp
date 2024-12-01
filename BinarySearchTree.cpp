936e5da8f1979b96ca4808d1aeae61b3eeeab1bc
marybadalyan <marybadalyangdh@gmail.com>
2024-12-01 20:23:31 +0400

    added erase iterative  function

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -2,12 +2,18 @@
 
 template <typename T>
 class BinarySearchTree{
+
 public:
-    BinarySearchTree(TreeNode<T>* r = nullptr): tree_root(r){};
+    BinarySearchTree(TreeNode<T>* r = nullptr): tree_root(r){}
+
     ~BinarySearchTree(){
         clear();
     }
-    void insertIterative(T value){
+
+    bool isEmpty(){
+        return tree_root == nullptr;
+    }
+    void insertIterative(const T& value){
         if (tree_root == nullptr){
             tree_root = new TreeNode<T>(value);
             return;
@@ -35,22 +41,88 @@ public:
             else break;  //don't allow duplicates break in case there is more to be executed 
         }
     }
-    void insertRHelper(){
-        insertRecursive(tree_root);
+    void insertRecursive(const T& value){
+        tree_root = insertRecursive(tree_root,value);
     }
-    TreeNode<T>* insertRecursive(TreeNode<T>*root,T value){
+    
+    void eraseIterative(const T& v){
+        if (isEmpty()) {
+            throw std::underflow_error("Cannot erase from an empty tree.");
+            return;
+        }
+        //the easierst is deleteing a leaf 
+        TreeNode<T>* prev = nullptr;
+        TreeNode<T>* curr = tree_root;
+        T value = v; //this might change
+
+        while(curr != nullptr){
+
+            if(value < curr->value){
+                prev = curr;
+                curr = curr->left;
+            }
+            else if(value > curr->value){
+                prev = curr;
+                curr = curr->right;
+            }
+            else{
+                if(curr->left == nullptr && curr->right == nullptr){
+                    if(curr->value > prev->value){
+                        prev->right = nullptr;
+                    }
+                    else{
+                        prev->left = nullptr
+                    }
+                    delete curr;
+                    curr = nullptr;
+                    return;
+                }
+                if(curr->right == nullptr){ // means curr->left isnt null cus the previous condition would have been right
+                    if(curr->value < prev->value){
+                        prev->left = curr->left;
+                    }
+                    else{
+                        prev->right = curr->left;
+                    }
+
+                    delete curr;
+                    curr = nullptr;
+                    return;
+                } 
+                if(curr->left == nullptr){
+                    if(curr->value > prev->value){
+                        prev->right = curr->right;
+                    }
+                    else{
+                        prev->left = curr->right;
+                    }
+                }
+
+                TreeNode<T>* min = FindMinNode(curr->right);
+                curr->value = min->value;
+                value = min->value;
+                prev = curr;
+                curr = curr->right;  //lets find the minimum node and delete it   
+            }
+        }
+    }
+
+
+private:
+    TreeNode<T>* insertRecursiveHelper(TreeNode<T>*root, constr T& value){ // or you can pass a poiner by reference  without returning every root 
         if(root == nullptr){
            return new TreeNode<T>(value);
         }
         if(value < root->value){
-            root->left= insertRecursive(root->left,value);
+            root->left= insertRecursiveHelper(root->left,value);
         }
         else if(value > root->value){
-            root->right = insertRecursive(root->right,value)
+            root->right = insertRecursiveHelper(root->right,value);
         }
-        else return;
+
         return root;
     }
+  
 private:
     TreeNode<T>* tree_root;
 };
